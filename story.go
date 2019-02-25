24d7c33ccef383a2fae052028a1a8803f0b1a25e
Ravneet Singh <Ravneet.Singh01@sap.com>
2019-02-24 23:07:46 -0500

    Update story.go

--- story.go
+++ story.go
@@ -4,36 +4,79 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"text/template"
 )
 
 // default HTML template
 var tmpl *template.Template
 
+const defaultHTML = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ .Title }} - Choose Your Own Adventure</title>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align: center;
+        position: relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #fffcf6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{ .Title }}</h1>
+      {{ range .Paragraphs }}
+      <p>{{.}}</p>
+      {{ end }}
+      <ul>
+        {{ range .Options }}
+        <li>
+          <a href="/{{ .Chapter }}">{{ .Text }}</a>
+        </li>
+        {{ end }}
+      </ul>
+    </section>
+  </body>
+</html>`
+
 func init() {
-	filename := "story.tpl"
-
-	f, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bytes, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	s := string(bytes)
-
-	tmpl = template.Must(template.New("").Parse(s))
-}
-
-type handler struct {
-	story     Story
-	template  *template.Template
-	parsePath func(r *http.Request) string
+	tmpl = template.Must(template.New("").Parse(defaultHTML))
 }
 
 // parsePath gets the chapter title from a request's URL path.
@@ -43,6 +86,12 @@ func parsePath(r *http.Request) string {
 		path = "/intro"
 	}
 	return path[1:]
+}
+
+type handler struct {
+	story     Story
+	template  *template.Template
+	parsePath func(r *http.Request) string
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
